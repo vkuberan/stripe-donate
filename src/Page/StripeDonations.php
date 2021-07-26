b458e02aa418a6f6d5798da99c18ed21a360e433
vkuberan@gmail.com <vkuberan@outlook.com>
2021-07-27 03:54:14 +0530

    Update StripeDonations.php

--- src/Page/StripeDonations.php
+++ src/Page/StripeDonations.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace VeeKay\StripeDonate\Page;
-
+  
 
 /**
  * Class StripeSettings
@@ -10,51 +10,50 @@ namespace VeeKay\StripeDonate\Page;
  */
 class StripeDonations extends AbstractPage implements PageInterface {
 
-	public function __construct() {
-	}
+  public function __construct() {
+  }
 
-	/**
-	 * @return string
-	 */
-	public function get_page_title() {
-		return esc_html__( 'All Donations', 'stripe-donate' );
-	}
+  /**
+   * @return string
+   */
+  public function get_page_title() {
+    return esc_html__( 'All Donations', 'stripe-donate' );
+  }
 
-	/**
-	 * @return string
-	 */
-	public function get_menu_title() {
-		return esc_html__( 'All Donations', 'stripe-donate' );
-	}
+  /**
+   * @return string
+   */
+  public function get_menu_title() {
+    return esc_html__( 'All Donations', 'stripe-donate' );
+  }
 
-	/**
-	 * Return the static slug string.
-	 *
-	 * @return string
-	 */
-	public function get_slug() {
-		return 'stripe-all-donations';
-	}
+  /**
+   * Return the static slug string.
+   *
+   * @return string
+   */
+  public function get_slug() {
+    return 'stripe-all-donations';
+  }
 
-	/**
-	 * Callback function for menu item
-	 */
-	public function render() {
-		require_once dirname(__DIR__) . '/templates/all-donations.php';
-	}
+  /**
+   * Callback function for menu item
+   */
+  public function render() {
+    require_once dirname(__DIR__) . '/templates/all-donations.php';
+  }
 
-	/**
-	 * @return string
-	 */
-	protected function get_submit_button_title() {
-		return esc_html__( 'Update', 'stripe-donate' );
-	}
+  /**
+   * @return string
+   */
+  protected function get_submit_button_title() {
+    return esc_html__( 'Update', 'stripe-donate' );
+  }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function save() {
-    
+  /**
+   * {@inheritdoc}
+   */
+  public function save() {
   }
 
 }
