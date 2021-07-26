6bb492e2b66288c056f384b4435c424575a79f78
vkuberan@gmail.com <vkuberan@outlook.com>
2021-07-27 03:48:04 +0530

    Update Manager.php

--- src/Page/Manager.php
+++ src/Page/Manager.php
@@ -29,98 +29,98 @@ class Manager {
     );
   }
 
-	public function display_page() {
+  public function display_page() {
 
-		if ( !current_user_can( 'manage_options' ) )  {
-			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-		}
-		require_once dirname(__DIR__) . '/templates/credits.php';
+    if ( !current_user_can( 'manage_options' ) )  {
+      wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+    }
+    require_once dirname(__DIR__) . '/templates/credits.php';
 
-	}
+  }
 
-        /**
-	 * Add page.
-	 *
-	 * @param PageInterface $page
-	 */
-	public function add_page( PageInterface $page ) {
-		$this->pages[ $page->get_slug() ] = $page;
-	}
+  /**
+   * Add page.
+   *
+   * @param PageInterface $page
+   */
+  public function add_page( PageInterface $page ) {
+    $this->pages[ $page->get_slug() ] = $page;
+  }
 
-	/**
-	 * Handling the POST-Request and save the data.
-	 */
-	public function save() {
+  /**
+   * Handling the POST-Request and save the data.
+   */
+  public function save() {
 
-		if ( $_SERVER[ 'REQUEST_METHOD' ] !== 'POST' ) {
-			return;
-		}
+    if ( $_SERVER[ 'REQUEST_METHOD' ] !== 'POST' ) {
+      return;
+    }
 
-		$page = filter_input( INPUT_POST, 'action' );
-		if ( '' === $page ) {
-			return;
-		}
+    $page = filter_input( INPUT_POST, 'action' );
+    if ( '' === $page ) {
+      return;
+    }
 
-		if ( ! isset( $this->pages[ $page ] ) ) {
-			return;
-		}
+    if ( ! isset( $this->pages[ $page ] ) ) {
+      return;
+    }
 
-		if ( ! check_admin_referer( 'stripe-donate-settings', 'stripe-donate' ) ) {
-			return;
-		}
+    if ( ! check_admin_referer( 'stripe-donate-settings', 'stripe-donate' ) ) {
+      return;
+    }
 
-		/** @var PageInterface */
-		$this->pages[ $page ]->save();
-	}
+    /** @var PageInterface */
+    $this->pages[ $page ]->save();
+  }
 
-	/**
-	 * Register all Pages.
-	 *
-	 * @wp-hook admin_menu
-	 */
-	public function register_pages() {
+  /**
+   * Register all Pages.
+   *
+   * @wp-hook admin_menu
+   */
+  public function register_pages() {
 
-		$this->add_new_menu();
+    $this->add_new_menu();
 
-		foreach ( $this->pages as $slug => $page ) {
+    foreach ( $this->pages as $slug => $page ) {
 
-			/**
-			 * @param string        $cap
-			 * @param PageInterface $page
-			 */
-			$cap = apply_filters( 'insr-capability', 'manage_options', $page );
+      /**
+       * @param string        $cap
+       * @param PageInterface $page
+       */
+      $cap = apply_filters( 'insr-capability', 'manage_options', $page );
 
-			add_submenu_page(
-				'stripe-donate',
-				$page->get_page_title(),
-				$page->get_menu_title(),
-				$cap,
-				$slug,
-				[ $this, 'render' ]
-			);
-		}
+      add_submenu_page(
+        'stripe-donate',
+        $page->get_page_title(),
+        $page->get_menu_title(),
+        $cap,
+        $slug,
+        [ $this, 'render' ]
+      );
+    }
 
-		global $submenu;
-		$submenu['stripe-donate'][0][0] = 'Home';
-	}
+    global $submenu;
+    $submenu['stripe-donate'][0][0] = 'Home';
+  }
 
-	/**
-	 * Render all pages and handling save too.
-	 */
-	public function render() {
+  /**
+   * Render all pages and handling save too.
+   */
+  public function render() {
 
-		$current_page = isset( $_GET[ 'page' ] ) ? $_GET[ 'page' ] : key( $this->pages );
+    $current_page = isset( $_GET[ 'page' ] ) ? $_GET[ 'page' ] : key( $this->pages );
 
-		$output = '<div class="wrap">';
+    $output = '<div class="wrap">';
 	
-		// Set the current page.
-		$page = $this->pages[ $current_page ];
+    // Set the current page.
+    $page = $this->pages[ $current_page ];
 
-		echo '<h1 id="title">' . esc_html__( $page->get_page_title(), 'stripe-donate' ) . '</h1>';
-		$this->save();
-		$page->display_errors();
-		$page->render();
-		echo '</div>'; 
-	}
+    echo '<h1 id="title">' . esc_html__( $page->get_page_title(), 'stripe-donate' ) . '</h1>';
+    $this->save();
+    $page->display_errors();
+    $page->render();
+    echo '</div>'; 
+  }
 
 }
